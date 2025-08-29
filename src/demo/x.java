a4ec48a1910e1f08539539d148250dbf6e7f9876
AbhijeetHellan <abhijeethellan427@gmail.com>
2025-08-29 01:43:55 -0400

    Update x.java

--- src/demo/x.java
+++ src/demo/x.java
@@ -1,7 +1,7 @@
 package demo;
-
 public class x {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         System.out.println("Hello World");
     }
 }
