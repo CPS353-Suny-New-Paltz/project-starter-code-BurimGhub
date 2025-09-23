06df40f53eed7ce48894863ede1d41cb573b19ac
BurimGhub <burimgashi2005@gmail.com>
2025-09-23 19:58:07 -0400

    Fixing Checkstyle

--- test/apitesting/NetworkAPIImplTest.java
+++ test/apitesting/NetworkAPIImplTest.java
@@ -1,6 +1,8 @@
 package apitesting;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Tag;
