1bc76714521b3d1fd2390da4d8bcdbe8bde216e5
BurimGhub <burimgashi2005@gmail.com>
2025-09-22 22:20:58 -0400

    Checkpoint 3 implementation for processapi

---
+++ src/emptyimplementations/IntegerStreamImpl.java
@@ -0,0 +1,15 @@
+package emptyimplementations;
+
+import java.util.List;
+
+import processapi.IntegerStream;
+
+public class IntegerStreamImpl implements IntegerStream {
+
+	@Override
+	public List<Integer> getNumbers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
